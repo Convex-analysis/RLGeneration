e26f0c7c90930d419ec2d18cd6552877f8cc0457
Convex-analysis <799447128@qq.com>
2024-07-24 11:28:52 -0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   ACER_files/resource_allocation/stability/ACER256_resource_allocation_log_13.csv
    	new file:   ACER_files/resource_allocation/stability/ACER32_resource_allocation_log_15.csv
    	new file:   ACER_files/resource_allocation/stability/ACER64_resource_allocation_log_14.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_10.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_11.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_3.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_4.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_5.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_6.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_7.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_8.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_9.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_h256.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_h32.csv
    	new file:   ACER_files/resource_allocation/stability/ACER_resource_allocation_log_h64.csv
    	new file:   ACER_preTrained/resource_allocation/ACER32_resource_allocation_0_0.pth
    	new file:   SUMOsimulationStep.txt
    	new file:   Victoria.sumocfg
    	new file:   VictoriaMap.net.xml
    	new file:   VictoriaMap.osm
    	new file:   VictoriaRoute.rou.xml
    	new file:   __pycache__/env.cpython-38.pyc
    	new file:   __pycache__/env.cpython-39.pyc
    	new file:   acer.py
    	new file:   env.py
    	new file:   output_file.xml
    	new file:   output_parser.py
    	new file:   plot.py
    	new file:   randomTrips.py
    	new file:   trips.trips.xml
    	new file:   vehicle_settings.csv

---
+++ output_parser.py
@@ -0,0 +1,75 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import pandas as pd
+import numpy as np
+
+Domtree = xml.dom.minidom.parse("output_file.xml")
+collection = Domtree.documentElement
+trips = collection.getElementsByTagName("tripinfo")
+#initialize the 4d numpy array
+nparray = np.array([["id", "depart", "duration", "arrival","train", "communication", "alpha"]])
+#given the required epoch number
+epochs = 10
+<<<<<<< HEAD
+#set the random seed
+np.random.seed(0)
+#iterate through all the nodes in the xml file
+=======
+
+>>>>>>> 366109950b4bd22652aa8b4257d35660ef0e4034
+for node in trips:
+    #get the values of the attributes
+    v_id = node.getAttribute("id")
+    depart = node.getAttribute("depart")
+    duration = node.getAttribute("duration")
+    arrival = node.getAttribute("arrival")
+    
+    #transform the string to float
+    duration = float(duration)
+<<<<<<< HEAD
+    #print("duration: ", duration)
+=======
+    print("duration: ", duration)
+>>>>>>> 366109950b4bd22652aa8b4257d35660ef0e4034
+    #let the sum of communication time and computation time be randomly generated from 0.5 to 1.5 times run_time
+    total_time =  np.random.randint(duration *0.5, duration *1.5)
+
+    #the ratio of communication time to computation time is randomly generated from 0.5 to 0.9
+<<<<<<< HEAD
+    #ratio = np.random.uniform(0.6, 0.9)
+    communication_time = np.random.normal(20, 5)
+    train_time = np.random.normal(120, 20)
+=======
+    ratio = np.random.uniform(0.6, 0.9)
+    communication_time = total_time * ratio
+    train_time = total_time - communication_time
+>>>>>>> 366109950b4bd22652aa8b4257d35660ef0e4034
+    #generate the least epoch number accoding to the given epochs and a guassian distribution
+    least_epoch = np.random.normal(epochs*0.5, 3)
+    if least_epoch <= 1:
+        least_epoch = 1
+    elif least_epoch > epochs:
+        least_epoch = epochs
+    else:
+        least_epoch = round(least_epoch)
+    print("least_epoch: ", least_epoch)
+    alpha = int(epochs/least_epoch)
+    nparray = np.append(nparray, [[v_id, depart, duration, arrival,train_time, communication_time, alpha]], axis = 0)
+    
+    #wirte the output to a csv file
+    
+#delete the first row of the array
+dfcolumns = nparray[0]
+nparray = np.delete(nparray, 0, 0)    
+df = pd.DataFrame(nparray, columns = dfcolumns)
+#sort the dataframe by the depart time
+df = df.sort_values(by = "depart")    
+df.to_csv("vehicle_settings.csv", index = False)
+
+
+    
+    
+
+
+    
+    
---
+++ plot.py
@@ -0,0 +1,23 @@
+import matplotlib 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Plotting function from a csv file to a 2d plot
+def plot(filename,x, y):
+    #load data from csv file as a dataframe
+    data = pd.read_csv(filename)
+    #extract the column "episode" as x-axis and "reward" as y-axis
+    x = data[x]
+    y = data[y]
+    #plot the data
+    plt.plot(x, y)
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.title('Reward vs Episode')
+    plt.show()
+
+if __name__ == "__main__":
+    path = "ACER_files/resource_allocation/stability/"
+    filename = "ACER_resource_allocation_log_11.csv"
+    plot(path + filename, "episode", "reward")
