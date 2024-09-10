b1ef70995bbedeb11940ef369328813e48a663f4
convex-analysis <799447128@qq.com>
2024-09-10 16:01:04 -0700

    Changes to be committed: 	new file:   lowdensity/low.sh 	modified:   plot.py

--- plot.py
+++ plot.py
@@ -13,6 +13,32 @@ highwithfilter = [781.3,580,20]#average watting time=118
 #from highdensity\highresult.csv
 highDSR_results = [619.88,533,19.993]#no waiting time,surplus=1
 
+def high_density_time_bar_plot():
+    labels = ['Without \n Filter', 'Without Filter \n (Surplus = 2)', 'With Filter', 'DSR']
+    x = np.arange(len(labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, [highwithoutfilter[0], highwithoutfiltersurplus[0], highwithfilter[0], highDSR_results[0]], width, label='Average Round Wall Time')
+    rects2 = ax.bar(x + width/2, [highwithoutfilter[1], highwithoutfiltersurplus[1], highwithfilter[1], highDSR_results[1]], width, label='Longest Schedule Vehicle')
+    ax.set_ylabel('Time')
+    ax.set_title('Comparison of Different Scheduling Strategies')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
+
+def high_density_number_bar_plot():
+    labels = ['Without \n Filter', 'Without Filter \n (Surplus = 2)', 'With Filter', 'DSR']
+    x = np.arange(len(labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, [highwithoutfilter[2], highwithoutfiltersurplus[2], highwithfilter[2], highDSR_results[2]], width, label='Scheduled Number')
+    ax.set_ylabel('Number')
+    ax.set_title('Comparison of Different Scheduling Strategies')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
 
 # Plotting function from a csv file to a 2d plot
 def plot(filename,x, y):
@@ -31,4 +57,5 @@ def plot(filename,x, y):
 if __name__ == "__main__":
     path = "ACER_files/resource_allocation/stability/"
     filename = "ACER_resource_allocation_log_9.csv"
-    plot(path + filename, "episode", "reward")
+    #plot(path + filename, "episode", "reward")
+    high_density_number_bar_plot()
