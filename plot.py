70c1de48ce7a5bb742815eb28fbd2b04caa2d898
convex-analysis <799447128@qq.com>
2024-09-10 15:09:48 -0700

    Changes to be committed: 	modified:   constantCSscheme.py 	new file:   highdensity/constantCSscheme_09-10-14-23.csv 	new file:   highdensity/constantCSscheme_09-10-14-27.csv 	new file:   highdensity/constantCSscheme_09-10-14-30.csv 	new file:   highdensity/constantCSscheme_09-10-14-37.csv 	new file:   highdensity/constantCSscheme_09-10-14-40.csv 	new file:   highdensity/constantCSscheme_09-10-14-41.csv 	renamed:    result.csv -> highdensity/highresult.csv 	modified:   plot.py

--- plot.py
+++ plot.py
@@ -3,6 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#[current_round_period, longest_schedule_vhicle, scheduled_number]
+#from highdensity\constantCSscheme_09-10-11-37.csv
+highwithoutfilter = [792.34,201,11.3]#average watting time=213.33,surplus=1
+#from highdensity\constantCSscheme_09-10-14-27.csv
+highwithoutfiltersurplus = [993.658,415,23]#average watting time=232,surplus=2
+#from highdensity\constantCSscheme_09-10-14-37.csv
+highwithfilter = [781.3,580,20]#average watting time=118
+#from highdensity\highresult.csv
+highDSR_results = [619.88,533,19.993]#no waiting time,surplus=1
+
+
 # Plotting function from a csv file to a 2d plot
 def plot(filename,x, y):
     #load data from csv file as a dataframe
